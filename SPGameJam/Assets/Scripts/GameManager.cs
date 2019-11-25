9c2d14dc83da71f1b230f3190f9644f0e4149ec4
fiwon123 <felipeinoue@yahoo.com.br>
2019-11-25 20:35:19 -0300

    Finish Elements

--- SPGameJam/Assets/Scripts/GameManager.cs
+++ SPGameJam/Assets/Scripts/GameManager.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-	public GameObject[] ObjectsPrefabs;
+	public GameObject[] elementsPrefabs;
+
 	public Animator sunAnim;
+
+	private int countObjects = 0;
+
+	private bool flagSun = false;
+
+	public GameObject starExplosion;
+	public GameObject cometaExplosion;
 
 	public void StartGame() {
 		StartCoroutine(StartSpawn());
@@ -13,9 +21,35 @@ public class GameManager : MonoBehaviour
 
 	IEnumerator StartSpawn() {
 		while (true) {
-			yield return new WaitForSeconds(1000);
 
-			//Instantiate(ObjectsPrefabs[0], ,,)
+			if (countObjects < 10) {
+				float x = Random.Range(0f, Screen.width);
+				float y = Random.Range(0f, Screen.height);
+
+				Vector3 newPosition = Camera.main.ScreenToWorldPoint(new Vector3(x, y));
+				newPosition.z = 0f;
+
+				Instantiate(elementsPrefabs[0], newPosition, Quaternion.identity);
+				countObjects++;
+			}
+
+			yield return new WaitForSeconds(2);
+		}
+	}
+
+	public void SpawnObject(MatcherObject.type type, int level, Vector3 newPosition) {
+		countObjects -= 2;
+		switch (type) { 
+			case MatcherObject.type.ELEMENT:
+				if (level < 5) {
+					Instantiate(elementsPrefabs[level], newPosition, Quaternion.identity);
+				} else if (!flagSun){
+					Instantiate(elementsPrefabs[level], Vector3.zero, Quaternion.identity);
+					flagSun = true;
+				} else {
+					Instantiate(starExplosion, newPosition, Quaternion.identity);
+				}
+				break;
 		}
 	}
 }
---
+++ SPGameJam/Assets/Scripts/MatcherObject.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatcherObject : MonoBehaviour
+{
+	[Range(0, 4)]
+	public int level = 0;
+
+	public enum type { ELEMENT };
+	public type id;
+
+	public GameObject combineExplosion;
+
+	bool isHolding = false;
+
+	private void Update() {
+		if (Input.GetMouseButton(0) && isHolding) {
+			Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			newPosition.z = 0;
+			transform.position = newPosition;
+		}
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision) {
+		if (collision.tag == "Interact") {
+			MatcherObject other = collision.GetComponent<MatcherObject>();
+
+			if (other.id == id && other.level == level && isHolding) {
+				FindObjectOfType<GameManager>().SpawnObject(id, level+1, transform.position);
+				Instantiate(combineExplosion, transform.position, Quaternion.identity);
+				Destroy(collision.gameObject);
+				Destroy(gameObject);
+			}
+		}
+
+	}
+
+	private void OnMouseDown() {
+		isHolding = true;
+	}
+
+	private void OnMouseUp() {
+		isHolding = false;
+	}
+}
